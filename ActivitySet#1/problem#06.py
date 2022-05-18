e5e533872c5cdd92f8a969a31220cc8ad07c5023
DhanyaPadasalgi <96468239+DhanyaPadasalgi@users.noreply.github.com>
2022-05-18 10:24:45 +0530

    Update problem#06.py
    check program
    program not done

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -2,15 +2,22 @@
 
 largest = None
 smallest = None
-
-while True:
-    num = input("Enter a number? ")
-
+try:
+ while True:
+    num = input("Enter a number: ")
     if num == "done":
         break
-
-    # ...
-
+    elif largest is None or smallest is None:
+        num=int(num)
+        if num>largest:
+            largest=num
+            break
+        elif num<smallest:
+            smallest=num
+            break
     print(num)
 
-print("Maximum", largest)
+ print("Maximum", largest)
+
+except:
+    print("Invaid input")
