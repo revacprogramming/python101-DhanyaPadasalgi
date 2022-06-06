3021e734b9e236dd95e3a59f7b70413812ab9254
DhanyaPadasalgi <96468239+DhanyaPadasalgi@users.noreply.github.com>
2022-06-06 23:56:13 +0530

    Update problem#09.py
    done

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,20 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+#filename = "dataset/romeo.txt"
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    line.rstrip
+    x=line.split()
+    
+    for n in range (len(x)):
+        if x[n] not in lst:
+            lst.append(x[n])
+     
+lst.sort()
+# print(lst.sort())
+print(lst)
+            
+            
+     
