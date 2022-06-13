bbf5db4deb8153ce745cdbea785760f9d1eeeb0f
DhanyaPadasalgi <96468239+DhanyaPadasalgi@users.noreply.github.com>
2022-06-13 16:15:22 +0530

    Update problem#10.py

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,34 @@
 # Dictionaries
 
 filename = "dataset/mbox-short.txt"
+# Dictionaries
+
+name = "mbox-short.txt"
+
+# name = input("Enter file name: ")
+name = name.strip()
+fh = open(name)
+counts=dict()
+s=0
+maxval=0
+for line in fh:
+    if line.startswith("From "):
+        words=line.split()
+    
+        # word=words[1:2]
+        word = words[1]
+        # print(word)
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+
+    if counts[word]>=maxval:
+        highest=word
+            
+print(word, counts[word])
+            
+#num=list(count.values())
+#maxval=num.max()
+
+
