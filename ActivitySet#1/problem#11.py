6306eede527d2b5b9d9716f868689e4ef46b773f
DhanyaPadasalgi <96468239+DhanyaPadasalgi@users.noreply.github.com>
2022-06-27 10:28:16 +0530

    not done
    file does not itrate through lines
    check again

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,28 @@
 # Tuples
 
-filename = "dataset/mbox-short.txt"
+fname = input("Enter file name: ")
+fname = fname.strip()
+fh = open(fname,'r')
+counts=dict()
+for line in fh:
+    #print(line)
+    if line.startswith("From"):
+        words=(line.split())
+        #print(words)
+        time=words[5]
+        y=time.split(":")
+        print(y)
+        word=y[0]
+        #print(word)
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+        print(counts)
+
+
+counts=counts.sort()
+print(counts)
+
+            
+       
