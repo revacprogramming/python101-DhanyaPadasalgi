29de32efd24da741afacaed3824dfa73a42402aa
DhanyaPadasalgi <96468239+DhanyaPadasalgi@users.noreply.github.com>
2022-06-27 12:38:02 +0530

    run

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,15 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+#Actual data: http://py4e-data.dr-chuck.net/regex_sum_1546947.txt
+fname=input("enter file name")
+fname=fname.strip()
+sum=0
+import re
+fh=open(assingment.txt)
+for line in fh:
+      line=line.strip()
+      x=re.findall('([0-9]*$[735])',line)
+      x=int(x)
+      #print(x)
+      sum +=x
+print(sum)
